6d094e3f9cecb6182951ffde994d651618292957
KayCodes360 <khutsomachaka500@gmail.com>
2026-05-14 20:01:03 +0200

    Element Storer
    A C Program that reads user input for elements in an array and prints them at the end

---
+++ storeelements.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int elements[10] = {0};
+
+    printf("Read and Print elements of an array:\n");
+    printf("------------------------------------\n");
+
+    printf("Input 10 elements in the array:\n");
+
+    for(int i = 0; i < 10; i++){
+        printf("Element %d: ", i);
+        scanf("%d", &elements[i]);
+    }
+
+    printf("\nElements in the array are: ");
+    for(int i = 0; i < 10; i++){
+        printf("%d ", elements[i]);
+    }
+
+    return 0;
+}
