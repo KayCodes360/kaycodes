2f7856bf6337cb954f197008cebd7030c03c8eec
KayCodes360 <khutsomachaka500@gmail.com>
2026-05-14 20:57:34 +0200

    Element Sorting
    A C Program that reads user input for elements in array and sorts them in ascending order to print them

---
+++ elementsorting.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+    int size = 0;
+
+    printf("Sort the elements in an array in ascending order\n");
+    printf("---------------------------------------------\n");
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int elements[size];
+
+    printf("Input %d elements of the array: \n", size);
+
+    for(int i = 0; i < size; i++){
+        printf("Element - %d: ", i);
+        scanf("%d", &elements[i]); 
+    }
+
+    for(int i = 0; i < size - 1; i++){
+        for(int j = 0; j < size - 1 - i; j++){
+            if(elements[j] > elements[j + 1]){
+                int temp = elements[j];
+                elements[j] = elements[j + 1];
+                elements[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("The sorted array in ascending order is: \n");
+
+    for(int i = 0; i < size; i++){
+        printf("%d ", elements[i]);
+    }
+    
+    return 0;
+}
